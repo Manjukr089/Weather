4e2a9b3dccc6e8b95bb9696b53f2da75ef89f86f
Manjukr089 <95536866+Manjukr089@users.noreply.github.com>
2023-01-20 15:26:15 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,24 @@
+// weather api link
+// http://api.openweathermap.org/data/2.5/weather?q=bangalore&units=metric&appid=1cb6532aea3c298a830a71380eace21e
+
+let city = document.getElementById("city")
+let country = document.getElementById("country")
+let temparature = document.getElementById("temparature")
+let user_input = document.getElementById("user_input")
+
+let checkTemp = async () =>
+{
+    let cityValue = user_input.value
+    if(cityValue == "")
+    {
+        alert("Please enter city name before search")
+    }
+    else
+    {
+       let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`)
+       let data = await response.json()
+       city.textContent = data.name
+       country.textContent = data.sys.country
+       temparature.textContent = data.main.temp
+    }
+}
